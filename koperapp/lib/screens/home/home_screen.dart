06c2186fc5ab3fcbd9aae0001c3b7b705c631810
Tuchan <tuchanlp@gmail.com>
2021-10-16 20:57:19 +0200

    Register screen + other fixes... ig

--- koperapp/lib/screens/home/home_screen.dart
+++ koperapp/lib/screens/home/home_screen.dart
@@ -13,7 +13,10 @@ class HomeScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.switch_account),
             onPressed: () {
-              LoginScreen();
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
             },
           ),
         ],
@@ -21,6 +24,7 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: SfCalendar(
           view: CalendarView.workWeek,
+          dataSource: MeetingDataSource(_getDataSource()),
           firstDayOfWeek: 1,
           timeSlotViewSettings: TimeSlotViewSettings(
               nonWorkingDays: <int>[DateTime.sunday, DateTime.saturday]),
@@ -28,4 +32,56 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+List<Meeting> _getDataSource() {
+  final List<Meeting> meetings = <Meeting>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+      DateTime(today.year, today.month, today.day, 9, 15, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+  meetings.add(Meeting(
+      'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
 }
--- koperapp/lib/screens/home/login_screen.dart
+++ koperapp/lib/screens/home/login_screen.dart
@@ -7,73 +7,79 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.switch_account),
             onPressed: () {
-              HomeScreen();
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen()),
+              );
             },
           ),
         ],
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Center(
-                child: Image.asset(
-                  "assets/images/logo.png",
-                  height: MediaQuery.of(context).size.height / 3,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    height: MediaQuery.of(context).size.height / 3,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Login"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Hasło"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Zaloguj się"),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
-                    minimumSize: Size(MediaQuery.of(context).size.width - 50,
-                        MediaQuery.of(context).size.height / 15)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: OutlinedButton(
-                onPressed: () {},
-                child: Text(
-                  "Zarejestruj się",
-                  style: TextStyle(color: Colors.blueGrey),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Login"),
                 ),
-                style: OutlinedButton.styleFrom(
-                    side: BorderSide(color: Colors.blueGrey, width: 3.0),
-                    minimumSize: Size(MediaQuery.of(context).size.width - 50,
-                        MediaQuery.of(context).size.height / 15)),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Hasło"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Zaloguj się"),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.blueGrey,
+                      minimumSize: Size(MediaQuery.of(context).size.width - 50,
+                          MediaQuery.of(context).size.height / 15)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Zarejestruj się",
+                    style: TextStyle(color: Colors.blueGrey),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.blueGrey, width: 3.0),
+                      minimumSize: Size(MediaQuery.of(context).size.width - 50,
+                          MediaQuery.of(context).size.height / 15)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
